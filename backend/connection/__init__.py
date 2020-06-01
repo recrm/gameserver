6b121579d1a053bd62717668e7906cf7ac8260c4
Ryan Chartier <redrecrm@gmail.com>
2020-05-31 20:20:40 -0600

    reactivate all the games.

--- backend/connection/__init__.py
+++ backend/connection/__init__.py
@@ -2,10 +2,10 @@ from . import TicTacToe
 from . import Connect4
 
 managers = [
-#     Connect4.Connect4_4x4,
-#     Connect4.Connect4_5x4,
-#     Connect4.Connect4_5x5,
-#     TicTacToe.Tictactoe_3x3,
-#     TicTacToe.Tictactoe_4x4,
-    TicTacToe.Tictactoe_5x5,
+    Connect4.Connect4_4x4,
+    Connect4.Connect4_5x4,
+    Connect4.Connect4_5x5,
+    TicTacToe.Tictactoe_3x3,
+    TicTacToe.Tictactoe_4x4,
+#     TicTacToe.Tictactoe_5x5,
 ]
